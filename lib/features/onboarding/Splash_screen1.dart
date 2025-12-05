5d110040b81eafddb261ef5dbb3d0057827a2cef
Snehaa <94774019+sneehaa@users.noreply.github.com>
2025-12-05 16:33:14 +0545

    Splash screen created

---
+++ lib/features/onboarding/Splash_screen1.dart
@@ -0,0 +1,38 @@
+import 'package:agrix/features/onboarding_screen/Splash_screen2.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatefulWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  State<SplashScreen1> createState() => _SplashScreen1State();
+}
+
+class _SplashScreen1State extends State<SplashScreen1> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SplashScreen2()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5FFED),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width * 0.8,
+          ),
+          child: Image.asset("assets/images/logo.png", fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+}
