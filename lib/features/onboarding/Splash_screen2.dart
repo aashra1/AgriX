53f738ca7af728d3faded030b57fbaf821e1b834
Snehaa <94774019+sneehaa@users.noreply.github.com>
2025-12-05 16:33:51 +0545

    Splash screen created

---
+++ lib/features/onboarding/Splash_screen2.dart
@@ -0,0 +1,68 @@
+import 'package:agrix/features/onboarding_screen/Splash_screen3.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen2 extends StatefulWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  State<SplashScreen2> createState() => _SplashScreen2State();
+}
+
+class _SplashScreen2State extends State<SplashScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5FFED),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 400,
+              child: Image.asset("assets/images/splash2.png"),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                "Empowering Farmers with Smart Agriculture Solutions",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.crimsonPro(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SplashScreen3(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF0B3D0B),
+                minimumSize: const Size(230, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Text(
+                "Continue",
+                style: GoogleFonts.crimsonPro(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
