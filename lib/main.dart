d50cd3006f99655135eac5b748746635187e6dab
aashra1 <aashrapandey00@gmail.com>
2025-12-16 21:10:23 +0545

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-
+import 'package:agrix/core/theme/theme.dart';
 import 'package:agrix/features/onboarding/Splash_screen1.dart';
 import 'package:flutter/material.dart';
 
@@ -8,15 +8,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
       title: 'Agrix',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: const SplashScreen1(),
     );
   }
