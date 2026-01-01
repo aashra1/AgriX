934ed398e272ae4ec7889cbe48757faed18caf86
aashra1 <aashrapandey00@gmail.com>
2026-01-01 18:16:45 +0545

    Refactor Splash_screen3.dart with updated onboarding logic

--- lib/screens/onboarding/Splash_screen3.dart
+++ lib/screens/onboarding/Splash_screen3.dart
@@ -1,4 +1,5 @@
-import 'package:agrix/features/auth/login/login.dart';
+
+import 'package:agrix/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
